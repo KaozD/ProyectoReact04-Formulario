67c57c613114ca5f714aea9b832875d136011f20
carlos <carlosest.aguilarr@gmail.com>
2023-06-13 17:56:34 -0600

    Terminando Clase 2: Switches y Typography

--- src/Componentes/FormSignUp.jsx
+++ src/Componentes/FormSignUp.jsx
@@ -1,18 +1,26 @@
 import Button from "@mui/material/Button"
+import TextField from '@mui/material/TextField'
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+
+
+
 
 function FormSignUp (){
     return (
     <form>
-        <label >Nombre</label>
-        <input type="text" />
-        <label >Apellidos</label>
-        <input type="text" />
-        <label >Correo Electronico</label>
-        <input type="email" />
-        <label >Promociones</label>
-        <input type="checkbox" />
-        <label >Novedades</label>
-        <input type="checkbox" />
+        <TextField id="name" label="Nombre" variant="outlined" fullWidth margin="normal" />
+        <TextField id="lastName" label="Apellido" variant="outlined" fullWidth margin="normal" />
+        <TextField id="email" label="E-mail" variant="outlined" fullWidth margin="normal" />
+
+        <FormGroup >
+            
+            <FormControlLabel control={<Switch defaultChecked />} label="Promociones" />
+            <FormControlLabel control={<Switch defaultChecked />} label="Novedades" />
+
+        </FormGroup>
+            
         <Button variant="contained" color="success" >
             Registrarse
         </Button>
@@ -21,4 +29,17 @@ function FormSignUp (){
     )
 }
 
+
 export default FormSignUp;
+
+/*Elementos de Material UI y sus propiedades
+Cada elemento que nosotros deseemos instalar a nuestro proyecto, podremos en primera elegirlo desde la 
+pagina de Material UI, mui.com, en donde podremos ver el elemento y algunas variantes, asi como su codigo para 
+implementarlo.
+
+En dicha implementacion, debemos tener en cuenta que habra que poner su respectiva importacion, para que funcione
+el elemento deseado
+
+Y despues llamarlo como si fuera un componente, en este le podremos dar determinadas propiedades, que tambien podremos
+en la pagina de mui, al agregarle las caracteristicas llegaremos a un metoda sencillo para su codificacion.
+*/
